ba9413795ad86cb8b6091cec8922b7e625a14dde
Ryan Weaver <ryan@thatsquality.com>
2022-05-31 10:32:12 -0400

    Step 9: Stimulus: add convert action

--- assets/controllers/markdown_controller.js
+++ assets/controllers/markdown_controller.js
@@ -1,4 +1,9 @@
 import { Controller } from '@hotwired/stimulus';
+import snarkdown from 'snarkdown';
 
 export default class extends Controller {
+    convert(event) {
+        const rawText = event.currentTarget.value;
+        console.log(snarkdown(rawText));
+    }
 }
