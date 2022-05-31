123dfa4cbd1dfc84ee13c4f5926365c433430e2d
Ryan Weaver <ryan@thatsquality.com>
2022-05-31 10:32:15 -0400

    Step 22: LiveComponent: set up live model

--- src/Twig/SearchPackagesComponent.php
+++ src/Twig/SearchPackagesComponent.php
@@ -4,6 +4,7 @@ namespace App\Twig;
 
 use App\Repository\PackageRepository;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent('search_packages')]
@@ -11,6 +12,7 @@ class SearchPackagesComponent
 {
     use DefaultActionTrait;
 
+    #[LiveProp(writable: true)]
     public string $query = '';
 
     public function __construct(
